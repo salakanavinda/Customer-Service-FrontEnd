0e72e7b2da24553fe8d344fe6589e39bd9d2fb17
Salaka Navinda <mmsnavinda@gmail.com>
2019-06-28 14:59:20 +0530

    Add Customer Add functions to class

--- src/app/customer-create/customer-create.component.ts
+++ src/app/customer-create/customer-create.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CustomerService } from '../customer-service.service';
+import { Customer } from '../customer';
 
 @Component({
   selector: 'customer-create',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customer-create.component.css']
 })
 export class CustomerCreateComponent implements OnInit {
-
-  constructor() { }
+  
+  customer: Customer=new Customer();
+  submitted=false;
+  constructor(private customerService : CustomerService) { }
 
   ngOnInit() {
+  }
+  newCustomer(){
+    this.submitted=false;
+    this.customer=new Customer();
+  }
+  save(){
+    this.customerService.createCustomer(this.customer).subscribe(data=>console.log(data),error=>console.log(error));
+    this.customer=new Customer();
+  }
+
+  onSubmit(){
+    this.submitted=true;
+    this.save();
   }
 
 }
