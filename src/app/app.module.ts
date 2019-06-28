c971fc90bf2c72344daa4a07a541e56b7077450f
Salaka Navinda <mmsnavinda@gmail.com>
2019-06-28 14:46:44 +0530

    Update Httpclientmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,8 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
