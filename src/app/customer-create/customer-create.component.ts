48fc9123c1f27520e065793bdb98d44963268ecd
Salaka Navinda <mmsnavinda@gmail.com>
2019-07-05 10:22:41 +0530

    Update customer-create.component.ts

--- src/app/customer-create/customer-create.component.ts
+++ src/app/customer-create/customer-create.component.ts
@@ -10,23 +10,25 @@ import { Customer } from '../customer';
 export class CustomerCreateComponent implements OnInit {
   
   customer: Customer=new Customer();
-  submitted=false;
+  submitted=false;//boolean variable
+  //Inject the customer service into customer create componet class
   constructor(private customerService : CustomerService) { }
 
   ngOnInit() {
+    //start the customer-create component 
   }
   newCustomer(){
     this.submitted=false;
     this.customer=new Customer();
   }
-  save(){
+  save(){//method for push data to back end using http services
     this.customerService.createCustomer(this.customer).subscribe(data=>console.log(data),error=>console.log(error));
-    this.customer=new Customer();
+    this.customer=new Customer();//when saved customer initialize the customer as new customer by running newCustomer() method 
   }
 
   onSubmit(){
     this.submitted=true;
-    this.save();
+    this.save();//initialize the save method when click add button in Html template
   }
 
 }
