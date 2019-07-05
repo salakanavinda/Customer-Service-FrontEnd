949f4700df465e88c7705a794bdb5efaef9320ec
Salaka Navinda <mmsnavinda@gmail.com>
2019-07-05 10:39:17 +0530

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,8 @@ import { CustomerListComponent } from './customer-list/customer-list.component';
 import { CustomerCreateComponent } from './customer-create/customer-create.component';
 import { CustomerSearchComponent } from './customer-search/customer-search.component';
 
+
+//add routes arr 
 const routes: Routes = [
   {path:'Customers',component:CustomerListComponent},
   {path:'InsertCustomer',component:CustomerCreateComponent},
