0116e8dc2e72206557e1a5467248afb88409d015
Salaka Navinda <mmsnavinda@gmail.com>
2019-07-05 10:38:07 +0530

    Update customer-service.service.ts

--- src/app/customer-service.service.ts
+++ src/app/customer-service.service.ts
@@ -6,17 +6,21 @@ import { Observable } from 'rxjs';
   providedIn: 'root'
 })
 export class CustomerService {
-
+  //Initialize the base url of the related back end
   private baseUrl ='http://localhost:8083/cus-cloud/customer';
-  constructor(private http: HttpClient) { }
-
+  constructor(private http: HttpClient) { 
+  //here use the httpClient module for communuate to the back-end
+  }
+  
+  //method of get specific customer using ID
   getCustomer(id:number): Observable<Object>{
     return this.http.get(`${this.baseUrl}/${id}`);
   }
+  //method of push customer to the back end 
   createCustomer(customer: Object): Observable<Object> {
     return this.http.post(`${this.baseUrl}`,customer);
   }
-
+  //method of get customers from back end
   getCustomersList(): Observable<any> {
     return this.http.get(`${this.baseUrl}`);
   }
